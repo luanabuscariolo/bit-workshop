d239193116da7468f5442c63aaea01f24076fb9f
Luana Buscariolo <67836162+luanabuscariolo@users.noreply.github.com>
2026-08-27 21:27:54 +0100

    atualiza baseUrl e projectName para o novo nome do repositório

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,12 +15,12 @@ const config: Config = {
   },
 
   url: 'https://luanabuscariolo.github.io',
-  baseUrl: '/rovermind-nanogrump-course/',
+  baseUrl: '/bit-workshop/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'luanabuscariolo',
-  projectName: 'rovermind-nanogrump-course',
+  projectName: 'bit-workshop',
 
   onBrokenLinks: 'throw',
 
